d49e6edba70e9bbdaf9201c28474359e39069c68
chasem <cpmcquown@gmail.com>
2021-11-16 15:11:16 -0800

    adds additional styling

--- src/pages/about.js
+++ src/pages/about.js
@@ -23,7 +23,7 @@ function About() {
       <br/>
       <br/>
         <span className='font-link'>
-          <p className="about-p">Welcome! My name is Chase McQuown, and I'm a graduate of the Coding Boot Camp at the University of Texas (my certification is viewable via the certs tab). I love being outside, and my free time generally consists of fishing, snowboarding, skateboarding, or staring at the celing because my code isn't working. Anyways, thanks for stopping by! </p>
+          <p className="about-p">Welcome! My name is Chase McQuown, and I'm a graduate of the Coding Boot Camp at the University of Texas (my certification is viewable via the certs tab). I love being outside, and my free time generally consists of fishing, snowboarding, skateboarding, or staring at the ceiling because my code isn't working. Anyways, thanks for stopping by! </p>
         </span>
         </section>
     );
